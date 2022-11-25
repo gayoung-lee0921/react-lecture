efccffea7196d997c20104297636e88140573cdb
gayoung-lee0921 <dudnrkdud1@gmail.com>
2022-11-25 17:48:13 +0900

    lotto & 가위바위보

---
+++ javascript-basic/lotto/js/main.js
@@ -0,0 +1,58 @@
+const result = document.querySelector("#result");
+const radios = document.querySelectorAll(".radio");
+const colors = ["yellow", "blue", "red", "gray", "green"];
+
+function makeLotto() {
+  const ul = document.createElement("ul");
+  result.appendChild(ul);
+  const nums = Array(45) //45개 배열 생성
+    .fill(10) //채우겠다
+    .map(function (current, idx) {
+      return idx + 1;
+    });
+
+  const selectedNums = [];
+
+  for (let i = 0; i < 6; i++) {
+    const selected = Math.floor(Math.random() * nums.length);
+    selectedNums.push(nums.splice(selected, 1).pop());
+  }
+  selectedNums.sort(compare);
+  selectedNums.forEach(function (item, idx) {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.classList.add(colors[Math.ceil(item / 10) - 1]);
+    /*
+  if (item >= 1 && item <= 10) {
+    li.classList.add(colors[0]);
+  } else if (item >= 11 && item <= 20) {
+    li.classList.add(colors[1]);
+  } else if (item >= 21 && item <= 30) {
+    li.classList.add(colors[2]);
+  } else if (item >= 31 && item <= 40) {
+    li.classList.add(colors[3]);
+  } else {
+    li.classList.add(colors[4]);
+  }
+  */
+    ul.appendChild(li);
+  });
+}
+
+radios.forEach(function (item, idx) {
+  item.addEventListener("change", function () {
+    // result 안에 모든 내용들을 지움
+    result.innerHTML = "";
+    for (let i = 0; i < idx + 1; i++) {
+      makeLotto();
+    }
+  });
+});
+
+// 오름차순
+function compare(a, b) {
+  // if (a > b) return 1;
+  // else if (a < b) return -1;
+  // else return 0;
+  return a - b;
+}
--- javascript-basic/rock-paper-scissors/js/main.js
+++ javascript-basic/rock-paper-scissors/js/main.js
@@ -28,19 +28,76 @@
 //pseudo array(유사배열)
 const comList = document.querySelectorAll("#com ul li");
 const playerList = document.querySelectorAll("#player ul li");
+const result = document.querySelector("#result ul");
+const cover = document.querySelector("#cover");
+const resultTxt = cover.querySelector("h1 strong");
+const btnRestart = cover.querySelector("#btnRestart");
+let random = 0; // 변수 random을 전역변수로 설정
+let gamecount = 0;
+let clearComChoice = 0;
+let clearReset = 0;
+let winCount = 0;
+let drawCount = 0;
+let loseCount = 0;
+
+// ddd = 3, w= 1 & d=1 & l=1
+//w >=2, w=1 &&& d==2
 
 function playerChoice() {
   playerList.forEach(function (item, idx) {
     item.addEventListener("click", function () {
-      // console.log("click");
+      gamecount += 1;
+      document.body.classList.add("blocking");
       clearInterval(clearComChoice);
+      // 숫자가 아닌 값을 뽑고 싶을 때 방법
+      const selected = parseInt(this.dataset.id); //type casting(형 변환), Number() / parseInt() : 정수
+      decide(selected, random);
+      restartFunc();
+      if (gamecount >= 3) {
+        cover.classList.add("on");
+        clearInterval(clearComChoice);
+        clearTimeout(clearReset);
+        // 게임은 3판
+        if (drawCount === 3 || (winCount === 1 && drawCount === 1 && loseCount === 1)) {
+          resultTxt.textContent = "DRAW";
+        } else if (winCount >= 2 || (winCount >= 1 && drawCount >= 2)) {
+          resultTxt.textContent = "WIN";
+        } else {
+          resultTxt.textContent = "LOSE";
+        }
+      }
     });
   });
 }
 
+// 승패 분석
+function decide(playerNum, comNum) {
+  const li = document.createElement("li");
+  if (playerNum === comNum) {
+    li.textContent = "D";
+    li.classList.add("draw");
+    drawCount++;
+    // 웹 브라우저가 태그로 인식했음 좋겠다 .innerHTNL
+    //result.innerHTML += `<li class="draw">D</li>`;
+    //result.append(`<li class="draw">D</li>`);
+  } else if ((comNum === 0 && playerNum === 1) || (comNum === 1 && playerNum === 2) || (comNum === 2 && playerNum === 0)) {
+    li.textContent = "W";
+    li.classList.add("win");
+    //result.innerHTML = `<li class="win">W</li>`;
+    //result.append(`<li class="win">W</li>`);
+    winCount++;
+  } else {
+    li.textContent = "L";
+    li.classList.add("lose");
+    //result.innerHTML += `<li class="lose">L</li>`;
+    //result.append(`<li class="lose">L</li>`);
+    loseCount;
+  }
+  result.append(li);
+}
+
 function comChoice() {
-  const random = Math.floor(Math.random() * 3);
-  console.log(random);
+  random = Math.floor(Math.random() * 3);
   comList.forEach(function (item, idx) {
     if (idx === random) {
       item.style.display = "block";
@@ -51,8 +108,26 @@ function comChoice() {
     // idx === random ? (item.style.display = "block") : (item.style.display = "none");
   });
 }
-// comChoice();
 
-//자동으로 실행
-let clearComChoice = setInterval(comChoice, 50); //setInterval은 나중에 호출(비동기)
+// 완전 리셋 - 3게임 모두 끝났을 때
+function restart() {
+  cover.classList.remove("on");
+  gamecount = 0;
+  winCount = 0;
+  drawCount = 0;
+  loseCount = 0;
+  result.innerHTML = "";
+  restartFunc();
+}
+
+// 1게임 끝났을 때 리셋
+function restartFunc() {
+  clearReset = setTimeout(function () {
+    clearComChoice = setInterval(comChoice, 20);
+    document.body.classList.remove("blocking");
+  }, 1000);
+}
+
+btnRestart.addEventListener("click", restart);
 playerChoice();
+restart();
